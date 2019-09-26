db728aeb0fab90debdae2697e8c84e0da55a8171
Vitalik <nevadskiy@gmail.com>
2019-09-26 15:21:33 +0300

    T-742: add base service provider

---
+++ src/TranslatableServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Nevadskiy\Translatable;
+
+use Illuminate\Support\ServiceProvider;
+
+class TranslatableServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+
+    }
+
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        $this->bootMigrations();
+    }
+
+    /**
+     * Bootstrap package migrations.
+     *
+     * @return void
+     */
+    private function bootMigrations(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../migrations');
+    }
+}
