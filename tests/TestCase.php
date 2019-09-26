d991e8eba3e0e54f8b36a140f829c8f3c590c68a
Vitalik <nevadskiy@gmail.com>
2019-09-26 15:22:04 +0300

    T-742: init base package files

---
+++ tests/TestCase.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Nevadskiy\Translatable\Tests;
+
+use Illuminate\Foundation\Application;
+use Nevadskiy\Translatable\TranslatableServiceProvider;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+class TestCase extends OrchestraTestCase
+{
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+//        $this->loadMigrationsFrom(__DIR__ . '/Support/migrations');
+
+//        $this->artisan('migrate', ['--database' => 'testbench'])->run();
+    }
+
+    /**
+     * Get package providers.
+     *
+     * @param Application $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app): array
+    {
+        return [TranslatableServiceProvider::class];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param Application $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app): void
+    {
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+    }
+}
