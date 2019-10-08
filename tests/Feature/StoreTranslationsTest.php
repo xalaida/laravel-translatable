43692460ced6631ed772975f8fa5cf9268a36893
Vitalii Kalaida <nevadskiy@gmail.com>
2019-10-08 15:22:40 +0300

    T-742: add test for multiple attributes translation

--- tests/Feature/StoreTranslationsTest.php
+++ tests/Feature/StoreTranslationsTest.php
@@ -2,16 +2,16 @@
 
 namespace Nevadskiy\Translatable\Tests;
 
-use Nevadskiy\Translatable\Engine\GoogleTranslateEngine;
+use Mockery;
 use Nevadskiy\Translatable\Engine\TranslatorEngine;
 use Nevadskiy\Translatable\Tests\Support\Models\Book;
 
 class StoreTranslationsTest extends TestCase
 {
-    // TODO: add possibility to save many translations
     // TODO: check if attribute is translatable...
     // TODO: check if default locale
     // TODO: check if many attributes should only be from translatable array
+    // TODO: test attribute mutators
 
     /** @test */
     public function it_can_save_translations_for_translatable_attributes(): void
@@ -28,6 +28,28 @@ class StoreTranslationsTest extends TestCase
         app()->setLocale('ru');
 
         $this->assertEquals('Тестовое название книги', $book->title);
+    }
+
+    /** @test */
+    public function it_can_save_many_translations_for_translatable_attributes(): void
+    {
+        $book = new Book([
+            'title' => 'Test book title',
+            'description' => 'Test book description',
+        ]);
+
+        $book->save();
+
+        $book->translate([
+            'title' => 'Тестовое название книги',
+            'description' => 'Тестовое описание книги',
+        ], 'ru');
+
+        app()->setLocale('ru');
+
+        $this->assertCount(2, $book->translations);
+        $this->assertEquals('Тестовое название книги', $book->title);
+        $this->assertEquals('Тестовое описание книги', $book->description);
     }
 
     /** @test */
@@ -50,12 +72,35 @@ class StoreTranslationsTest extends TestCase
         $this->assertCount(1, $book->translations);
     }
 
-//    /** @test */
-//    public function it_google_translate(): void
-//    {
-//        $t = app(TranslatorEngine::class);
-//
-//        dd($t->translate('Test book title', 'ru', 'en'));
-//    }
+    /** @test */
+    public function it_can_use_translation_engine(): void
+    {
+        $book = new Book([
+            'title' => 'Test book title',
+            'description' => 'Test book description',
+        ]);
+
+        $book->save();
+
+        $engine = Mockery::mock(TranslatorEngine::class);
+
+        $engine->shouldReceive('translate')
+            ->with('Test book title', 'ru', 'en')
+            ->andReturn('Тестовое название книги');
+
+        $engine->shouldReceive('translate')
+            ->with('Test book description', 'ru', 'en')
+            ->andReturn('Тестовое описание книги');
+
+        $this->app->instance(TranslatorEngine::class, $engine);
+
+        $book->translateUsingEngine('ru');
+
+        app()->setLocale('ru');
+
+        $this->assertCount(2, $book->translations);
+        $this->assertEquals('Тестовое название книги', $book->title);
+        $this->assertEquals('Тестовое описание книги', $book->description);
+    }
 }
 
