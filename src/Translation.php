d338f97726f072e536421747b87578e8c7a43775
Vitalik <nevadskiy@gmail.com>
2019-10-24 15:59:03 +0300

    Add doc properties for model

--- src/Translation.php
+++ src/Translation.php
@@ -2,15 +2,22 @@
 
 namespace Nevadskiy\Translatable;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Support\Collection;
 
 /**
+ * @property int id
  * @property string locale
  * @property string translatable_attribute
  * @property string translatable_value
+ * @property int translatable_id
+ * @property string translatable_type
  * @property Collection translations
+ * @property HasTranslations translatable
+ * @property Carbon updated_at
+ * @property Carbon created_at
  */
 class Translation extends Model
 {
