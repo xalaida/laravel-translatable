361f45a52fea666efe3b3a874ff3dae9713446ef
Vitalii Kalaida <nevadskiy@gmail.com>
2020-04-28 12:03:44 +0300

    Add model mixin doc block

--- src/HasTranslations.php
+++ src/HasTranslations.php
@@ -2,9 +2,11 @@
 
 namespace Nevadskiy\Translatable;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 /**
+ * @mixin Model
  * @property Translation[] translations
  */
 trait HasTranslations
