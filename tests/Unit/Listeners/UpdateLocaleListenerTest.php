0f6f187a829550ddb3eebe4d954c30a60e867433
Nevadskiy <nevadskiy@gmail.com>
2020-05-10 00:40:19 +0300

    Add tests to locale listener

---
+++ tests/Unit/Listeners/UpdateLocaleListenerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Nevadskiy\Translatable\Tests\Unit;
+
+use Nevadskiy\Translatable\ModelTranslator;
+use Nevadskiy\Translatable\Tests\TestCase;
+
+class UpdateLocaleListenerTest extends TestCase
+{
+    /** @test */
+    public function it_updates_translator_locale_correctly(): void
+    {
+        $translator = $this->app->make(ModelTranslator::class);
+        $originalLocale = $translator->getLocale();
+
+        $this->app->setLocale('ru');
+
+        $this->assertEquals('ru', $translator->getLocale());
+        $this->assertNotEquals('ru', $originalLocale);
+    }
+}
