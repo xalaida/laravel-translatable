accb2161818041edb0ea0ed1fe371b033c75970a
nevadskiy <nevadskiy@gmail.com>
2021-01-13 18:14:43 +0200

    Feature preferred translations to be ordered first

--- src/Models/Translation.php
+++ src/Models/Translation.php
@@ -17,6 +17,7 @@ use Nevadskiy\Uuid\Uuid;
  * @property Model translatable
  * @property string value
  * @property string locale
+ * @property bool is_preferred
  * @property Carbon updated_at
  * @property Carbon created_at
  */
@@ -25,11 +26,30 @@ class Translation extends Model
     use Uuid;
 
     /**
+     * Perform any actions required after the model boots.
+     */
+    protected static function booted(): void
+    {
+        static::addGlobalScope('preferredFirst', static function (Builder $query) {
+            $query->preferredFirst();
+        });
+    }
+
+    /**
      * The attributes that aren't mass assignable.
      *
      * @var array
      */
     protected $guarded = [];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'is_preferred' => 'bool',
+    ];
 
     /**
      * The relationships that should be touched on save.
@@ -73,5 +93,13 @@ class Translation extends Model
     public function scopeForAttribute(Builder $query, string $attribute): Builder
     {
         return $query->where('translatable_attribute', $attribute);
+    }
+
+    /**
+     * Scope translations by the preferred translations first.
+     */
+    public function scopePreferredFirst(Builder $query): Builder
+    {
+        return $query->orderByDesc('is_preferred');
     }
 }
