4d0274d706d9a9c1b7937c6c2b7f94993d47846f
nevadskiy <nevadskiy@gmail.com>
2022-02-19 17:11:50 +0200

    WIP

--- src/HasEntityTranslations.php
+++ src/HasEntityTranslations.php
@@ -137,4 +137,47 @@ trait HasEntityTranslations
     {
         $this->translations()->delete();
     }
+
+    /**
+     * Retrieve the model for a bound value.
+     *
+     * @param mixed $value
+     * @param string|null $field
+     * @return Model|null
+     */
+    public function resolveRouteBinding($value, $field = null)
+    {
+        $field = $field ?? $this->getRouteKeyName();
+
+        if (! $this->shouldResolveRouteBindingUsingTranslations($field)) {
+            return parent::resolveRouteBinding($value, $field);
+        }
+
+        // TODO: feature resolving model by translatable attribute. (possible two ways: if missing use fallback or 404) so maybe do not implement this method.
+
+        $locale = $this->translation()->getLocale();
+
+        $model = $this->whereTranslatable($field, $value, $locale)->first();
+
+        if ($model) {
+            return $model;
+        }
+
+        return $this->newQuery()
+            ->where($field, $value)
+            ->whereDoesntHave('translations', function ($query) use ($field, $locale) {
+                $query->forAttribute($field);
+                $query->forLocale($locale);
+            })
+            ->first();
+    }
+
+    protected function shouldResolveRouteBindingUsingTranslations(string $field): bool
+    {
+        if (! $this->isTranslatable($field)) {
+            return false;
+        }
+
+        return ! $this->translation()->isFallbackLocale();
+    }
 }
