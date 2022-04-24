3bc03851dfd486cdf1a1b3ac9ca2f1ce072c212f
nevadskiy <nevadskiy@gmail.com>
2022-04-24 13:22:47 +0300

    Add testsuite for single strategy

--- aliases.sh
+++ aliases.sh
@@ -4,5 +4,8 @@ alias app='docker run --rm -it -v ${PWD}:/app app'
 # Test by filter alias
 alias tf='docker run --rm -it -v ${PWD}:/app app vendor/bin/phpunit --stop-on-failure --filter'
 
+# Test by testsuite alias
+alias ts='docker run --rm -it -v ${PWD}:/app app vendor/bin/phpunit --stop-on-failure --testsuite'
+
 # Test by filter with coverage report
 alias tfc='docker run --rm -it -v ${PWD}:/app app vendor/bin/phpunit --coverage-text --filter'
