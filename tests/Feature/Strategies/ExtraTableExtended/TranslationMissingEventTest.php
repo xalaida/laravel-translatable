2b8d29d5b173d057013851584411c4f03d113a26
nevadskiy <nevadskiy@gmail.com>
2022-06-11 23:47:05 +0300

    Test translation missing event

---
+++ tests/Feature/Strategies/ExtraTableExtended/TranslationMissingEventTest.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace Nevadskiy\Translatable\Tests\Feature\Strategies\ExtraTableExtended;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Event;
+use Nevadskiy\Translatable\Events\TranslationMissing;
+use Nevadskiy\Translatable\Exceptions\TranslationMissingException;
+use Nevadskiy\Translatable\Strategies\ExtraTableExtended\HasTranslations;
+use Nevadskiy\Translatable\Tests\TestCase;
+
+class TranslationMissingEventTest extends TestCase
+{
+    /**
+     * @inheritdoc
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->createSchema();
+    }
+
+    /**
+     * Set up the database schema.
+     */
+    private function createSchema(): void
+    {
+        $this->schema()->create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();
+            $table->timestamps();
+        });
+
+        $this->schema()->create('book_translations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('book_id');
+            $table->string('title')->nullable();
+            $table->string('locale');
+            $table->timestamps();
+        });
+    }
+
+    /** @test */
+    public function it_fires_event_when_translation_is_missing_for_given_attribute_and_locale(): void
+    {
+        $book = new BookForTranslationMissingEvent();
+        $book->title = 'Nature clock';
+        $book->save();
+
+        Event::fake(TranslationMissing::class);
+
+        self::assertEquals('Nature clock', $book->translator()->get('title', 'uk'));
+
+        Event::assertDispatched(TranslationMissing::class, static function (TranslationMissing $event) use ($book) {
+            return $event->attribute === 'title'
+                && $event->locale === 'uk'
+                && $event->model->is($book);
+        });
+    }
+
+    /** @test */
+    public function it_does_not_fire_event_when_on_fallback_locale(): void
+    {
+        $book = new BookForTranslationMissingEvent();
+        $book->save();
+
+        Event::fake(TranslationMissing::class);
+
+        self::assertNull($book->translator()->getFallback('title'));
+
+        Event::assertNotDispatched(TranslationMissing::class);
+    }
+
+    /** @test */
+    public function it_throw_translation_missing_exception_when_trying_to_get_missing_translation(): void
+    {
+        $book = new BookForTranslationMissingEvent();
+        $book->title = 'Nature clock';
+        $book->save();
+
+        $this->expectException(TranslationMissingException::class);
+
+        $book->translator()->getOrFail('title', 'uk');
+    }
+
+    /** @test */
+    public function it_does_not_fire_translation_missing_event_when_translation_is_found(): void
+    {
+        $book = new BookForTranslationMissingEvent();
+        $book->translator()->set('title', 'Nature clock', 'en');
+        $book->translator()->set('title', 'Годинник природи', 'uk');
+        $book->save();
+
+        Event::fake(TranslationMissing::class);
+
+        self::assertEquals('Годинник природи', $book->translator()->get('title', 'uk'));
+
+        Event::assertNotDispatched(TranslationMissing::class);
+    }
+
+    /** @test */
+    public function it_does_not_fire_translation_missing_event_when_translation_is_nullable(): void
+    {
+        $book = new BookForTranslationMissingEvent();
+        $book->translator()->set('title', 'Nature clock', 'en');
+        $book->translator()->set('title', null, 'uk');
+        $book->save();
+
+        Event::fake(TranslationMissing::class);
+
+        self::assertNull($book->translator()->get('title', 'uk'));
+
+        Event::assertNotDispatched(TranslationMissing::class);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function tearDown(): void
+    {
+        $this->schema()->drop('book_translations');
+        $this->schema()->drop('books');
+        parent::tearDown();
+    }
+}
+
+/**
+ * @property string title
+ */
+class BookForTranslationMissingEvent extends Model
+{
+    use HasTranslations;
+
+    protected $table = 'books';
+
+    protected $translatable = [
+        'title',
+    ];
+
+    protected function getEntityTranslationTable(): string
+    {
+        return 'book_translations';
+    }
+
+    protected function getEntityTranslationForeignKey(): string
+    {
+        return 'book_id';
+    }
+}
